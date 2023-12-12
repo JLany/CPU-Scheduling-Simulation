a61fd6261a9b5ef89ec5ebb601b463807ce85dc9
Michael Youssry <16242325+Black-Hack@users.noreply.github.com>
2023-12-12 17:59:22 +0200

    Add Shortest-Job-First Scheduler

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,29 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        final var processes = new ArrayList<Process>();
+        System.out.println("Shortest-Job-First Scheduler");
+        final var sjfProcesses = new ArrayList<Process>();
 
         // Example from the book page 270
-        processes.add(new Process(1, "P1", 0, 0, 8, 0));
-        processes.add(new Process(2, "P2", 1, 0, 4, 0));
-        processes.add(new Process(3, "P3", 2, 0, 9, 0));
-        processes.add(new Process(4, "P4", 3, 0, 5, 0));
+        sjfProcesses.add(new Process(1, "P1", 0, 0, 8, 0));
+        sjfProcesses.add(new Process(2, "P2", 1, 0, 4, 0));
+        sjfProcesses.add(new Process(3, "P3", 2, 0, 9, 0));
+        sjfProcesses.add(new Process(4, "P4", 3, 0, 5, 0));
 
-        final var sched = new SJFScheduler(processes, 0);
+        final var sjfSched = new SJFScheduler(sjfProcesses, 0);
+        sjfSched.start();
 
-        sched.start();
+        System.out.println("Shortest-Remaining-Time-First Scheduler");
+        final var srtfProcesses = new ArrayList<Process>();
+
+        // Example from the book page 270
+        srtfProcesses.add(new Process(1, "P1", 0, 0, 8, 0));
+        srtfProcesses.add(new Process(2, "P2", 1, 0, 4, 0));
+        srtfProcesses.add(new Process(3, "P3", 2, 0, 9, 0));
+        srtfProcesses.add(new Process(4, "P4", 3, 0, 5, 0));
+
+        final var srtfSched = new SJFScheduler(srtfProcesses, 0);
+        srtfSched.start();
+
     }
 }
---
+++ src/SJFScheduler.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class SJFScheduler extends Scheduler {
+
+    public SJFScheduler(List<Process> processes, int contextSwitchTime) {
+        super(processes, contextSwitchTime, new PriorityQueue<Process>(new Comparator<Process>() {
+            @Override
+            public int compare(Process a, Process b) {
+                if (a.getRemainingTime() != b.getRemainingTime()) {
+                    return a.getRemainingTime() - b.getRemainingTime();
+                }
+                if (a.getArrivalTime() != b.getArrivalTime()) {
+                    return a.getArrivalTime() - b.getArrivalTime();
+                }
+                return 0;
+            }
+        }));
+    }
+
+    @Override
+    protected boolean shouldDoContextSwitch() {
+        return false;
+    }
+
+}
