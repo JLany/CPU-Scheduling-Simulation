1819bd7a71fa820860fa89e9c719141eff43550a
Michael Youssry <16242325+Black-Hack@users.noreply.github.com>
2023-12-12 16:59:10 +0200

    Add Shortest-Job-First Scheduler

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        final var processes = new ArrayList<Process>();
 
+        // Example from the book page 270
+        processes.add(new Process(1, "P1", 0, 0, 8, 0));
+        processes.add(new Process(2, "P2", 1, 0, 4, 0));
+        processes.add(new Process(3, "P3", 2, 0, 9, 0));
+        processes.add(new Process(4, "P4", 3, 0, 5, 0));
+
+        final var sched = new SJFScheduler(processes, 0);
+
+        sched.start();
     }
 }
